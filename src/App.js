d46a1eb6c9d899108f9ab59869e1bd3942bf0ebe
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-10 08:38:27 -0700

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [pepCheck, setPepCheck] = useState(false);
+
+  const togglePep = (e) => setPepCheck(e.target.checked);
+
   return (
-    <div></div>
-  )
-}
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={pepCheck}
+        aria-checked={pepCheck}
+        onChange={togglePep}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepCheck ? <li>Pepperoni</li> : null}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
